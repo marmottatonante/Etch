d66aa0c719b32a84f5b5a6f62117564f38177afd
marmottatonante <tommaso.detommaso@protonmail.com>
2026-02-19 15:03:01 +0100

    Introduced RenderOnce, RenderAt and RenderFree

--- Renderer.cs
+++ Renderer.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Security.Cryptography;
 
 namespace Etch;
 
@@ -11,23 +12,27 @@ public sealed class Renderer : IDisposable
     public double DeltaTime { get; private set; } = 0;
     public double FPS => 1.0 / DeltaTime;
 
+    public IControl? Root { get; set; }
+
     public Renderer() => Console.Write("\x1b[?25l\x1b[2J");
     public void Dispose() => Console.Write("\x1b[?25h\x1b[2J\x1b[H");
 
-    public void Render(IControl root)
+    public void RenderOnce()
     {
+        if (Root is null) return;
+
         _canvas.Clear();
-        Int2 size = root.Measure((Console.WindowWidth, Console.WindowHeight));
-        root.Render(new Context(_canvas, new(Int2.Zero, size)));
+        Int2 size = Root.Measure((Console.WindowWidth, Console.WindowHeight));
+        Root.Render(new Context(_canvas, new(Int2.Zero, size)));
         
         _output.Write(_canvas.Span);
         _output.Flush();
     }
 
-    public void Run(IControl root, int target)
+    public void RenderAt(int targetFPS)
     {
         Stopwatch stopwatch = new();
-        double targetSeconds = 1.0 / target;
+        double targetSeconds = 1.0 / targetFPS;
         
         bool isRunning = true;
         Console.CancelKeyPress += (_, e) => { e.Cancel = true; isRunning = false; };
@@ -35,12 +40,27 @@ public sealed class Renderer : IDisposable
         while (isRunning)
         {
             stopwatch.Restart();
-            Render(root);
+            this.RenderOnce();
 
             double elapsed = stopwatch.Elapsed.TotalSeconds;
             double remaining = targetSeconds - elapsed;
             if(remaining > 0) Thread.Sleep(TimeSpan.FromSeconds(remaining));
 
+            DeltaTime = stopwatch.Elapsed.TotalSeconds;
+        }
+    }
+
+    public void RenderFree()
+    {
+        Stopwatch stopwatch = new();
+        
+        bool isRunning = true;
+        Console.CancelKeyPress += (_, e) => { e.Cancel = true; isRunning = false; };
+
+        while (isRunning)
+        {
+            stopwatch.Restart();
+            this.RenderOnce();
             DeltaTime = stopwatch.Elapsed.TotalSeconds;
         }
     }
