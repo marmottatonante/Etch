d60a457e7d84741074af2dabf43521ee0765badf
marmottatonante <tommaso.detommaso@protonmail.com>
2026-02-23 13:53:25 +0100

    Moved buffer writing to its own ANSI class and optimized ANSI codes assembling

--- ANSI.cs
+++ ANSI.cs
@@ -1,14 +1,45 @@
+using System.Buffers;
 using System.Text;
 
 namespace Etch;
 
 public static class ANSI
 {
-    public static readonly byte[] Reset = "\x1b[0m"u8.ToArray();
-    public static readonly byte[] Bold = "\x1b[1m"u8.ToArray();
-    public static readonly byte[] Underline = "\x1b[4m"u8.ToArray();
-    public static byte[] MoveTo(Int2 position) => 
-        Encoding.UTF8.GetBytes($"\x1b[{position.Y + 1};{position.X + 1}H");
-    public static byte[] Color(int foreground, int background) => 
-        Encoding.UTF8.GetBytes($"\x1b[38;5;{foreground}m\x1b[48;5;{background}m");
+    public static void Reset(ArrayBufferWriter<byte> buffer) => buffer.Write("\x1b[0m"u8);
+    private static void Color(ArrayBufferWriter<byte> buffer, ReadOnlySpan<byte> prefix, Color color)
+    {
+        byte bColor = (byte)color;
+        var span = buffer.GetSpan(11);
+        int written = 0;
+        prefix.CopyTo(span[written..]); written += prefix.Length;
+        bColor.TryFormat(span[written..], out int fw, default, null); written += fw;
+        span[written++] = (byte)'m';
+        buffer.Advance(written);
+    }
+    public static void Foreground(ArrayBufferWriter<byte> buffer, Color color) => ANSI.Color(buffer, "\x1b[38;5;"u8, color);
+    public static void Background(ArrayBufferWriter<byte> buffer, Color color) => ANSI.Color(buffer, "\x1b[48;5;"u8, color);
+    public static void Move(ArrayBufferWriter<byte> buffer, Int2 position)
+    {
+        // \x1b[{row};{col}H
+        // Max length: "\x1b[" (2) + 5 digit row + ";" + 5 digit col + "H" = 14 bytes
+        var span = buffer.GetSpan(14);
+        int written = 0;
+        "\x1b["u8.CopyTo(span[written..]); written += 2;
+        (position.Y + 1).TryFormat(span[written..], out int rw, default, null); written += rw;
+        span[written++] = (byte)';';
+        (position.X + 1).TryFormat(span[written..], out int cw, default, null); written += cw;
+        span[written++] = (byte)'H';
+        buffer.Advance(written);
+    }
+    public static void Write(ArrayBufferWriter<byte> buffer, byte b)
+    {
+        buffer.GetSpan(1)[0] = b;
+        buffer.Advance(1);
+    }
+    public static void Write(ArrayBufferWriter<byte> buffer, ReadOnlySpan<char> text)
+    {
+        int bytes = Encoding.UTF8.GetMaxByteCount(text.Length);
+        var span = buffer.GetSpan(bytes);
+        buffer.Advance(Encoding.UTF8.GetBytes(text, span));
+    }
 }
