91a08a778e38bfcb4d902cc379c7562e89d4b10d
marmottatonante <tommaso.detommaso@protonmail.com>
2026-03-19 17:31:09 +0100

    Fixed Benchmark. It now uses Etch.Profiler.

--- Etch.Demo/Examples.cs
+++ Etch.Demo/Examples.cs
@@ -19,7 +19,7 @@ public static class Examples
                                (Console.WindowWidth, Console.WindowHeight));
 
         var title = new Label("Hello");
-        var subtitle = new Label("World").Under(title);
+        var subtitle = new Label("World").BottomOf(title);
         helloWorld.Add(title).Add(subtitle).Render();
 
         Console.ReadKey();
@@ -36,38 +36,63 @@ public static class Examples
                               (Console.WindowWidth, Console.WindowHeight));
 
         var title = new Label("Benchmarking");
-        var progress = new Progress(0, 10).Under(title, alignment: Layouts.Alignment.Center);
+        var progress = new Progress(0, 10).BottomOf(title, alignment: Layouts.Alignment.Center);
         benchmark.Add(title).Add(progress).Render();
         benchmark.Metrics.Active = true;
 
         var sw = Stopwatch.StartNew();
-        double totalDraw = 0;
-        double totalFlush = 0;
-
         int iterations = 0;
         while (sw.Elapsed.TotalSeconds < 10)
         {
             progress.Current.Value = sw.Elapsed.TotalSeconds;
             benchmark.Render();
-
-            totalDraw += benchmark.Metrics.DrawTime;
-            totalFlush += benchmark.Metrics.FlushTime;
             iterations++;
         }
-
+        benchmark.Metrics.Active = false;
         sw.Stop();
+
+        Console.Clear();
 
         static string Ms(double seconds) => $"{seconds * 1000:F3}ms";
         static string Sec(double seconds) => $"{seconds:F3}s";
-        static string Fps(double seconds) => $"{1.0 / seconds:F0}fps";
+        static string Fps(double seconds) => $"{1.0 / seconds}FPS";
 
-        double avgDraw = totalDraw / iterations;
-        double avgFlush = totalFlush / iterations;
-        double avgDelta = avgDraw + avgFlush;
+        Scene results = new(Console.OpenStandardOutput(), 
+                            (Console.WindowWidth, Console.WindowHeight));
 
-        Console.Clear();
-        Console.WriteLine($"Average Draw: {Ms(avgDraw)} - Average Flush: {Ms(avgFlush)} - Average Delta: {Ms(avgDelta)}");
-        Console.WriteLine($"Total Draw: {Sec(totalDraw)} - Total Flush: {Sec(totalFlush)} - Total Delta: {Sec(totalDraw + totalFlush)}");
-        Console.WriteLine($"Score: {iterations}");
+        // Header row
+        var h0 = new Label("");
+        var h1 = new Label("Total").RightOf(h0);
+        var h2 = new Label("Avg").RightOf(h1, 2);
+        var h3 = new Label("Avg FPS").RightOf(h2, 2);
+
+        // Draw row
+        var d0 = new Label("Draw").BottomOf(h0);
+        var d1 = new Label(Sec(benchmark.Metrics.TotalDraw)).RightOf(d0, 2);
+        var d2 = new Label(Ms(benchmark.Metrics.AverageDraw)).RightOf(d1, 2);
+        var d3 = new Label(Fps(benchmark.Metrics.AverageDraw)).RightOf(d2, 2);
+
+        // Flush row
+        var f0 = new Label("Flush").BottomOf(d0);
+        var f1 = new Label(Sec(benchmark.Metrics.TotalFlush)).RightOf(f0, 2);
+        var f2 = new Label(Ms(benchmark.Metrics.AverageFlush)).RightOf(f1, 2);
+        var f3 = new Label(Fps(benchmark.Metrics.AverageFlush)).RightOf(f2, 2);
+
+        // Delta row
+        var dl0 = new Label("Delta").BottomOf(f0);
+        var dl1 = new Label(Sec(benchmark.Metrics.TotalDelta)).RightOf(dl0, 2);
+        var dl2 = new Label(Ms(benchmark.Metrics.AverageDelta)).RightOf(dl1, 2);
+        var dl3 = new Label(Fps(benchmark.Metrics.AverageDelta)).RightOf(dl2, 2);
+
+        var score = new Label($"Score: {iterations}").BottomOf(dl0, 1);
+
+        results.Add(h0).Add(h1).Add(h2).Add(h3)
+               .Add(d0).Add(d1).Add(d2).Add(d3)
+               .Add(f0).Add(f1).Add(f2).Add(f3)
+               .Add(dl0).Add(dl1).Add(dl2).Add(dl3)
+               .Add(score)
+               .Render();
+
+        Console.ReadKey();
     }
 }
