a3a018827a7ea6b72f7143e42f37c3b741a292a5
marmottatonante <tommaso.detommaso@protonmail.com>
2026-04-07 18:16:43 +0200

    Streamlined Command code

--- Etch/Drawing/Command.cs
+++ Etch/Drawing/Command.cs
@@ -5,41 +5,18 @@ namespace Etch.Drawing;
 
 public readonly struct Command(Command.Type type, Arena<byte>.Handle handle)
 {
-    public enum Type : byte
-    {
-        Move,
-        Plot,
-        Blit,
-        Blank,
-        Foreground,
-        Background,
-    }
-
-    public readonly Type Kind = type;
-    public readonly Arena<byte>.Handle Handle = handle;
+    public enum Type : byte { Move, Plot, Blit, Blank, Foreground, Background }
 
     public void Execute(ArrayBufferWriter<byte> output, Arena<byte> artifacts)
     {
-        switch (Kind)
+        switch (type)
         {
-            case Type.Move:
-                ANSI.Move(output, artifacts.Read<Int2>(Handle)[0]);
-                break;
-            case Type.Plot:
-                ANSI.Plot(output, artifacts.Read(Handle)[0]);
-                break;
-            case Type.Blit:
-                ANSI.Blit(output, artifacts.Read(Handle));
-                break;
-            case Type.Blank:
-                ANSI.Blank(output, artifacts.Read<int>(Handle)[0]);
-                break;
-            case Type.Foreground:
-                ANSI.Foreground(output, artifacts.Read<Color>(Handle)[0]);
-                break;
-            case Type.Background:
-                ANSI.Background(output, artifacts.Read<Color>(Handle)[0]);
-                break;
+            case Type.Move: ANSI.Move(output, artifacts.Read<Int2>(handle)[0]); break;
+            case Type.Plot: ANSI.Plot(output, artifacts.Read(handle)[0]); break;
+            case Type.Blit: ANSI.Blit(output, artifacts.Read(handle)); break;
+            case Type.Blank: ANSI.Blank(output, artifacts.Read<int>(handle)[0]); break;
+            case Type.Foreground: ANSI.Foreground(output, artifacts.Read<Color>(handle)[0]); break;
+            case Type.Background: ANSI.Background(output, artifacts.Read<Color>(handle)[0]); break;
         }
     }
 }
